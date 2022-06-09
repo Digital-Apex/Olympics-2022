20c6062e69575beb7f7dc5b74e6fdbb4d3b08474
Joseph Rios <administrador@Josephs-MacBook-Pro.local>
2022-06-09 11:26:37 -0500

    fix

--- candidatos/forms.py
+++ candidatos/forms.py
@@ -4,7 +4,7 @@ from .models import *
 class CandidatoForm(forms.ModelForm):
     class Meta:
         model = CandidatoPresidente
-        fields = ['nombre','apellidos','edad','cedula','profesion', 'partido']
+        fields = ['nombre','apellidos','edad','cedula','profesion', 'partido', 'url', 'propuesta_1', 'propuesta_2', 'propuesta_3']
         
 class PropuestaForm(forms.ModelForm):
     class Meta:
